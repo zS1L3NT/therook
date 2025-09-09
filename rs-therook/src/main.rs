a348b6e3683794a5da063bc138429c96c1db5d60
zS1L3NT Mac <dev@zectan.com>
2025-09-09 20:03:35 +0800

    ✨ reset board to initial position

--- rs-therook/src/main.rs
+++ rs-therook/src/main.rs
@@ -5,19 +5,11 @@ use engine::*;
 pub use therook::*;
 
 fn main() {
-    // let mut board = Board::initial();
-    // let board = Board::fen("4k3/1p4pp/2p5/8/q3r2Q/3p3P/1P4PK/4R3 b - - 0 1".into()).unwrap();
-    let board = Board::fen("R1r1k3/1p4pp/2p5/8/Q3r2q/3p3P/1P4PK/4R3 b - - 0 1".into()).unwrap();
+    let board = Board::initial();
     println!("{board:?}");
 
-    println!("{:?}", board.pin_lines[board.turn]);
-    // board.move_piece(tile!(E2), tile!(E4), WHITE_PAWN);
-    // board.move_piece(tile!(D7), tile!(D5), BLACK_PAWN);
-    // println!("{board:?}");
-
-    for r#move in board.calculate_moves() {
-        println!("{move:?}");
-    }
+    let moves = board.calculate_moves();
+    println!("Moves[{}]: {moves:?}", moves.len());
 }
 
 #[macro_export]
